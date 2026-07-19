b96ce0c2d60cdcd0c934639a2b0c8e2b50b9f285
maestroaayush <kriprayush@gmail.com>
2026-07-19 17:39:09 +0545

    Backend implementation part 2

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,43 @@
-from fastapi import FastAPI
+// https://github.com/locususer/Day-1-frontendfrom 
 
-api = FastAPI()
+fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-@api.get("/")
-def hello():
-    return {"message": "Hello, I am a FastAPI server!"}
+app= FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_methods=["*"],
+    allow_headers =["*"]
+)
+
+#{
+#    id: 1,
+#    name: "Coffee",
+#    amount: 100
+# }
+
+expenses =[]
+next_id =1
+
+@app.get("/expenses")
+def get_expenses():
+    return expenses
+
+
+@app.post("/expenses")
+def add_expense(expense:dict):
+    global next_id
+    expense["id"]=next_id
+    next_id+=1
+    expenses.append(expense)
+    return expense
+
+
+# @app.delete("/expenses/{expense_id}")
+# def delete_expense(expense_id:int):
+#     for expense in expenses:
+#         if expense["id"]==expense_id:
+#             expenses.remove(expense)
+#             return{"message":"expense deleted"}
+#     return {"message":" expense not found"}
